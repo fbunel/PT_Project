36ea15c9eac99b3c7c8a683d746bea595392a893
hadriman <hadriman@live.fr>
2016-11-07 15:58:06 +0100

    tab -> espaces

--- Code/LCMC.py
+++ Code/LCMC.py
@@ -1,22 +1,22 @@
 class latticeMC:
 
-	def __init__(self, size, dim, angleSize, temperature)
-	"""Constructeur de la classe qui initialise :
-	-la lattice à l'aide de la classe discreteLattice
-	-la température
-	-l'énergie ?"""
+    def __init__(self, size, dim, angleSize, temperature)
+    """Constructeur de la classe qui initialise :
+    -la lattice à l'aide de la classe discreteLattice
+    -la température
+    -l'énergie ?"""
 
-	def runMC(self)
-	"""Fonction qui lance une simulation Monte-Carlo"""
+    def runMC(self)
+    """Fonction qui lance une simulation Monte-Carlo"""
 
-	def energie(self)
-	"""Fonction qui renvoie l'énergie de la configuration actuelle"""
+    def energie(self)
+    """Fonction qui renvoie l'énergie de la configuration actuelle"""
 
-	def orderParameter(self)
-	"""Fonction qui calcule la valeur du paramètre d'ordre"""
+    def orderParameter(self)
+    """Fonction qui calcule la valeur du paramètre d'ordre"""
 
-	def boltzmannWeight(self)
-	"""Fonction qui calcule le poid de Boltzmann d'une configuration"""
+    def boltzmannWeight(self)
+    """Fonction qui calcule le poid de Boltzmann d'une configuration"""
 
-	def partitionFunction(self)
-	"""Fonction qui renvoie la fonction de partition"""
+    def partitionFunction(self)
+    """Fonction qui renvoie la fonction de partition"""
--- Code/discreteLattice.py
+++ Code/discreteLattice.py
@@ -4,31 +4,31 @@ import numpy as np
 
 class discreteLattice(lattice):
 
-	def __init__(self, size, dim, angleSize):
-		"""Constructeur de la classe qui herite de lattice :
-		-construit un objet lattice a partir de size et dim
-		-le nombre d'orientations autorisees pour un site"""
+    def __init__(self, size, dim, angleSize):
+        """Constructeur de la classe qui herite de lattice :
+        -construit un objet lattice a partir de size et dim
+        -le nombre d'orientations autorisees pour un site"""
 
-		lattice.__init__(self, size, dim)
-		self.angleSize = angleSize
+        lattice.__init__(self, size, dim)
+        self.angleSize = angleSize
 
-	def randomOrientation(self):
-		"""Fonction qui renvoie une orientation aleatoire parmis celles autorisees."""
-		if self.dim==2 :
-			return (np.pi/angleSize*
-				np.array([randint(0,angleSize-1)]))
-		elif self.dim==3 :
-			return (np.pi/angleSize*
-				np.array([randint(0,angleSize-1),randint(0,angleSize-1)]))
+    def randomOrientation(self):
+        """Fonction qui renvoie une orientation aleatoire parmis celles autorisees."""
+        if self.dim==2 :
+            return (np.pi/angleSize*
+                np.array([randint(0,angleSize-1)]))
+        elif self.dim==3 :
+            return (np.pi/angleSize*
+                np.array([randint(0,angleSize-1),randint(0,angleSize-1)]))
 
 
-	def changeOrientation(self, loc, angle):
-		"""Fonction qui change l'orientation d'un site a une nouvelle valeur angle."""
-		
-		if dim==2 :
-			self.latticeArray[loc[0],loc[1]] = angle
-		elif dim==3 :
-			self.latticeArray[loc[0],loc[1],loc[2]] = angle
+    def changeOrientation(self, loc, angle):
+        """Fonction qui change l'orientation d'un site a une nouvelle valeur angle."""
+        
+        if dim==2 :
+            self.latticeArray[loc[0],loc[1]] = angle
+        elif dim==3 :
+            self.latticeArray[loc[0],loc[1],loc[2]] = angle
 
-		
-	
+        
+    
